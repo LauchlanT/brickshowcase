57af568da9a2786d11f36d411039278ce9b75e59
Lauchlan Toal <lauchlantoal@gmail.com>
2020-09-27 05:58:12 +0200

    Added database access class

--- helpers/db.php
+++ helpers/db.php
@@ -1,3 +1,14 @@
 <?php
 
-//Database calls
+include_once("error.php");
+
+//Database class that makes all calls to the database
+class DatabaseAccessor {
+
+	private $pdo;
+	
+	public function __construct() {
+		include_once("connection.php");
+		$this->pdo = new PDO($dbdsn, $dbuser, $dbpass, $dbopt);
+	}
+}
